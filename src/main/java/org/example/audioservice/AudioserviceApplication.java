b63236e1b698b062c7b86708ca881aa7e04f0a0b
paulus.bangun <pbangunk@gmail.com>
2025-03-15 17:11:03 +0700

    init commit

---
+++ src/main/java/org/example/audioservice/AudioserviceApplication.java
@@ -0,0 +1,13 @@
+package org.example.audioservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AudioserviceApplication.class, args);
+    }
+
+}
