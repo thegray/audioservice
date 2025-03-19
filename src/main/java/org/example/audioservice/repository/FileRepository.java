0c85d40a288796263977e2bd90af20ab490d3688
paulus.bangun <pbangunk@gmail.com>
2025-03-19 08:10:54 +0700

    add find file functions in File repository

--- src/main/java/org/example/audioservice/repository/FileRepository.java
+++ src/main/java/org/example/audioservice/repository/FileRepository.java
@@ -4,6 +4,10 @@ import org.example.audioservice.model.FileEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FileRepository extends JpaRepository<FileEntity, Long> {
+    Optional<FileEntity> findByUserIdAndPhraseIdAndFormat(Long userId, Long phraseId, String format);
+    Optional<FileEntity> findByUserIdAndPhraseIdAndOriginal(Long userId, Long phraseId, boolean original);
 }
