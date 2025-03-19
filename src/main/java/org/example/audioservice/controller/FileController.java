245b40c395c98783829e740b5a61527b170cf6e1
paulus.bangun <pbangunk@gmail.com>
2025-03-19 08:12:13 +0700

    implement get audio file controller

--- src/main/java/org/example/audioservice/controller/FileController.java
+++ src/main/java/org/example/audioservice/controller/FileController.java
@@ -6,12 +6,17 @@ import org.example.audioservice.exception.UserNotFoundException;
 import org.example.audioservice.payload.ApiResponse;
 import org.example.audioservice.payload.FileResponse;
 import org.example.audioservice.service.FileService;
+import org.example.audioservice.util.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/v1/audio")
@@ -42,15 +47,6 @@ public class FileController {
 
         FileDTO fileDTO = fileService.saveAudioFile(file, userId, phraseId);
 
-//        //mock data
-//        long id = System.currentTimeMillis();
-//        String name = id + ".mp3";
-//        FileDTO fileDTO = FileDTO.builder()
-//                .fileId(id)
-//                .fileName(name)
-//                .filePath("/storage/audio/" + name)
-//                .build();
-
         FileResponse fileResponse = FileResponse.from(fileDTO);
         ApiResponse<FileResponse> response = ApiResponse.<FileResponse>builder()
                 .status("success")
@@ -62,13 +58,21 @@ public class FileController {
     }
 
     @GetMapping("/user/{userId}/phrase/{phraseId}/{audioFormat}")
-    public ResponseEntity<ApiResponse<String>> getAudioFile(@PathVariable String userId, @PathVariable String phraseId, @PathVariable String audioFormat) {
+    public ResponseEntity<byte[]> getAudioFile(@PathVariable Long userId, @PathVariable Long phraseId, @PathVariable String audioFormat) throws IOException {
         Log.info("getAudioFile|userId:{}, phraseId:{}, audioFormat:{}", userId, phraseId, audioFormat);
-        ApiResponse<String> response = ApiResponse.<String>builder()
-                .status("success")
-                .build();
 
-        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body(response);
+        // Validate user and phrase, similar to upload, maybe can create function
+
+        byte[] fileData = fileService.getAudioFile(userId, phraseId, audioFormat);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(FileUtils.getAudioMediaType(audioFormat));
+        headers.setContentLength(fileData.length);
+        headers.setContentDispositionFormData("attachment", phraseId + "." + audioFormat);
+
+        Log.info("getAudioFile|fileDataSize:{}", fileData.length);
+
+        return new ResponseEntity<>(fileData, headers, HttpStatus.OK);
     }
 
 }
