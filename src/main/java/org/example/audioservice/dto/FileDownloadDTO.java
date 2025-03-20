267766a9662578e76b4d7e802c844cd8b3520229
paulus.bangun <pbangunk@gmail.com>
2025-03-20 08:37:18 +0700

    add filedownloaddto for get audio service

--- src/main/java/org/example/audioservice/dto/FileDownloadDTO.java
+++ src/main/java/org/example/audioservice/dto/FileDownloadDTO.java
@@ -1,12 +1,11 @@
 package org.example.audioservice.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
 
 @Getter
 @SuperBuilder
-public class FileDTO {
-    private Long fileId;
-    private String fileName;
-    private String filePath;
+public class FileDownloadDTO extends FileDTO {
+    private byte[] file;
 }
