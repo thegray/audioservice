a7169ea3bdb15e52c9eea8054444ee9ef35c3af7
paulus.bangun <pbangunk@gmail.com>
2025-03-21 01:13:59 +0700

    add repository for get converted file wilth filters of userid, phraseid, format, groupid

--- src/main/java/org/example/audioservice/repository/FileRepository.java
+++ src/main/java/org/example/audioservice/repository/FileRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 @Repository
 public interface FileRepository extends JpaRepository<FileEntity, Long> {
     Optional<FileEntity> findTopByUserIdAndPhraseIdOrderByCreatedAtDesc(Long userId, Long phraseId);
+    Optional<FileEntity> findTopByUserIdAndPhraseIdAndFormatAndGroupIdOrderByCreatedAtDesc(Long userId, Long phraseId, String format, Long groupId);
 }
