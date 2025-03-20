3a40a877ac1fd5fb0eb1586b98ac66b0e8f8631a
paulus.bangun <pbangunk@gmail.com>
2025-03-21 01:10:50 +0700

    remove old repo, add get latest file by userid and phraseid

--- src/main/java/org/example/audioservice/repository/FileRepository.java
+++ src/main/java/org/example/audioservice/repository/FileRepository.java
@@ -8,6 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface FileRepository extends JpaRepository<FileEntity, Long> {
-    Optional<FileEntity> findTopByUserIdAndPhraseIdAndFormatOrderByCreatedAtDesc(Long userId, Long phraseId, String format);
-    Optional<FileEntity> findTopByUserIdAndPhraseIdAndOriginalOrderByCreatedAtDesc(Long userId, Long phraseId, boolean original);
+    Optional<FileEntity> findTopByUserIdAndPhraseIdOrderByCreatedAtDesc(Long userId, Long phraseId);
 }
